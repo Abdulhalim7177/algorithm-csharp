adb23345cbc2591fe58977ad97af3d5e2b8d804a
Abdulhalim Muhammad Yaro <123118801+Abdulhalim7177@users.noreply.github.com>
2024-09-27 16:35:07 +0100

    Binary Search.cs

---
+++ Binary Search.cs
@@ -0,0 +1,16 @@
+public static int BinarySearch(int[] array, int target)
+{
+    int left = 0, right = array.Length - 1;
+
+    while (left <= right)
+    {
+        int mid = left + (right - left) / 2;
+        if (array[mid] == target)
+            return mid;
+        else if (array[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
